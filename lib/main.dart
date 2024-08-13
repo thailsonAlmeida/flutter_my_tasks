9cd370da7b38f5ec6ffa79c3d3bd94fec84143d3
Thailson Almeida <103151779+thailsonAlmeida@users.noreply.github.com>
2024-08-12 21:18:23 -0300

    create widget user_create

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:my_tasks/login.dart';
+import 'package:my_tasks/user_create.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Login(),
+      home: UserCreate(),
     );
   }
 }
